22376e0bb4c263ba213fdd0e2b63af14c2ed7ea2
Dave Mateer <davemateer@gmail.com>
2015-08-19 07:06:28 +0100

    Adding in Functional2 which has 3 static methods composed together, and the start of tests

---
+++ FunctionalDemoDave/Functional2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FunctionalDemoDave
+{
+    public static class Functional2
+    {
+        private static void Main()
+        {
+            Action runProcessing = () => RunProcessing(Log, GetCustomers, CreateReport);
+            runProcessing();
+        }
+
+        public static void RunProcessing(
+            Action<string> log,
+            Func<IEnumerable<string>> getCustomers,
+            Func<string, string> createReport)
+        {
+            log("test");
+            var customers = getCustomers();
+            foreach (var customer in customers)
+            {
+                log(customer);
+                var report = createReport(customer);
+                log(report);
+            }
+        }
+
+        public static string CreateReport(string customerName)
+        {
+            return customerName + " done!";
+        }
+
+        public static IEnumerable<string> GetCustomers()
+        {
+            yield return "dave";
+            yield return "bob";
+            yield return "alice";
+        }
+
+        public static void Log(string message)
+        {
+            Console.WriteLine("log: {0}", message);
+        }
+    }
+}
---
+++ FunctionalDemoDave/Functional2Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FunctionalDemoDave
+{
+    public class Functional2Tests
+    {
+        [Fact]
+        public void FunctionalTest()
+        {
+            // arrange
+            var expectedCustomer = "ellie";
+            var expectedReportBody = "ellie done!";
+
+            // mocking out getCustomers
+            Func<IEnumerable<string>> getCustomers =
+                () => new[] { expectedCustomer };
+
+            Func<string, string> createReport = customer => expectedCustomer;
+
+            Action<string> log = m => Console.WriteLine(m);
+
+            // act
+            Functional2.RunProcessing(log, getCustomers, createReport);
+
+            // assert
+            //Assert.Equal(expectedCustomer.Email, actualToAddress);
+            //Assert.Equal(expectedReportBody, actualBody);
+        }
+    }
+}

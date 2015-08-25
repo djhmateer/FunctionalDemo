8ae865a9bf1a8750d1a87ee6eaa507b77ad7f979
Dave Mateer <davemateer@gmail.com>
2015-08-25 05:43:42 +0100

    A very simple clousre

---
+++ FunctionalDemoDave/Closure.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FunctionalDemoDave
+{
+    public static class Closure
+    {
+        private static void Main()
+        {
+            Console.Out.WriteLine("Closure test");
+
+            // this is creating a closure
+            // we are creating a Function that returns a string
+            // not acutally running it.  
+            // so when this goes out of scope (ie if we return IEnumerable<Customer> from Functional test
+            // will exptectedCustomer still be first@sea.com?? yes
+            // because the variable has been closed over.
+            //Func<string> x = DoSomething();
+            string x = DoSomething().Invoke();
+
+            Console.Out.WriteLine($"x is {x}");
+        }
+
+        static Func<string> DoSomething()
+        {
+            string thing = "asdf";
+            Func<string> getThingFunction = () => thing;
+            return getThingFunction;
+        }
+    }
+}
